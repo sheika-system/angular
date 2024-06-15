15d9f662362f0552055ca8b662f0eabf3bfb1de7
axlult <axlult@gmail.com>
2024-06-14 20:23:27 -0600

    first commit
    primer commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,69 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { AppLayoutComponent } from './components/app-layout/app-layout.component';
+import { SigUpComponent } from './pages/auth/sign-up/signup.component';
+import { UsersComponent } from './pages/users/users.component';
+import { AuthGuard } from './guards/auth.guard';
+import { AccessDeniedComponent } from './pages/access-denied/access-denied.component';
+import { AdminRoleGuard } from './guards/admin-role.guard';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { GuestGuard } from './guards/guest.guard';
+import { IRole } from './interfaces';
+
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [GuestGuard],
+  },
+  {
+    path: 'signup',
+    component: SigUpComponent,
+    canActivate: [GuestGuard],
+  },
+  {
+    path: 'access-denied',
+    component: AccessDeniedComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'app',
+    component: AppLayoutComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'app',
+        redirectTo: 'users',
+        pathMatch: 'full',
+      },
+      {
+        path: 'users',
+        component: UsersComponent,
+        canActivate:[AdminRoleGuard],
+        data: { 
+          authorities: [
+            IRole.admin, 
+            IRole.superAdmin
+          ],
+          name: 'Users'
+        }
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+        data: { 
+          authorities: [
+            IRole.admin, 
+            IRole.superAdmin,
+            IRole.user
+          ],
+          name: 'Dashboard'
+        }
+      }
+    ],
+  },
+];
---
+++ src/app/interfaces/index.ts
@@ -0,0 +1,41 @@
+export interface ILoginResponse {
+  accessToken: string;
+  expiresIn: number
+}
+
+export interface IResponse<T> {
+  data: T;
+}
+
+export interface IUser {
+  id?: number;
+  name?: string;
+  lastname?: string;
+  email?: string;
+  password?: string;
+  active?: boolean;
+  createdAt?: string;
+  updatedAt?: string;
+  authorities?: IAuthority[];
+}
+
+export interface IAuthority {
+  authority: string;
+}
+
+export interface IFeedBackMessage {
+  type?: IFeedbackStatus;
+  message?: string;
+}
+
+export enum IFeedbackStatus {
+  success = "SUCCESS",
+  error = "ERROR",
+  default = ''
+}
+
+export enum IRole {
+  admin = "ROLE_ADMIN",
+  user = "ROLE_USER",
+  superAdmin = 'ROLE_SUPER_ADMIN'
+}
