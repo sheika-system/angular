a525facedc821085a4fdf7bfd92b3afc74c0e041
axlult <axlult@gmail.com>
2024-07-23 10:49:59 -0600

    try5

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { MapComponent } from './app/components/map/MapComponent';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+  function initMap(): void {
+    new MapComponent('map', 'info');
+  }
+  
+  declare global {
+    interface Window { initMap: () => void; }
+  }
+  window.initMap = initMap;
