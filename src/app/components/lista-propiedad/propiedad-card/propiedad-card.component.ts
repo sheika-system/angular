4a4f0b336e0effc701a87402e4c7a0488c8f023e
adrDev0 <165166065+adrDev0@users.noreply.github.com>
2024-08-07 19:03:04 -0600

    Delete propietie working

--- src/app/components/lista-propiedad/propiedad-card/propiedad-card.component.ts
+++ src/app/components/lista-propiedad/propiedad-card/propiedad-card.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { IPropiedad } from '../../../interfaces';
 import { Router, RouterLink } from '@angular/router';
+import { PropiedadService } from '../../../services/propiedad.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-propiedad-card',
@@ -11,6 +13,8 @@ import { Router, RouterLink } from '@angular/router';
 })
 export class PropiedadCardComponent {
   @Input() propiedad!: IPropiedad;
+  private service = inject(PropiedadService)
+  private snackBar = inject(MatSnackBar);
 
   constructor(private router: Router) {}
 
@@ -20,5 +24,24 @@ export class PropiedadCardComponent {
 
   contactarPropietario(propiedadId: number | undefined){
     console.log('Contactando propietario: ' + propiedadId);
+  }
+
+  deletePropiedad(propiedad: IPropiedad) {
+    this.service.deletePropiedad(propiedad).subscribe({
+      next: () => {
+        this.snackBar.open('Propietie deleted', 'Close', {
+          horizontalPosition: 'right',
+          verticalPosition: 'top',
+          duration: 5 * 1000,
+        });
+      },
+      error: (error: any) => {
+        this.snackBar.open('Error deleting propietie', 'Close', {
+          horizontalPosition: 'right',
+          verticalPosition: 'top',
+          panelClass: ['error-snackbar']
+        });
+      }
+    })
   }
 }
